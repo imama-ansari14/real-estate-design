7abbe0db553466eb114c18410ec902a6b4bbc85f
Imama Ansari <ansariimama@yahoo.com>
2026-04-19 01:14:17 +0500

    remove icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
-
-import "./App.css";
 import SectionLuxury from "./components/SectionLuxury";
 import ServicesShowcase from "./components/ServicesShowcase";
 import StatsShowcasePro from "./components/StatsShowcasePro";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
