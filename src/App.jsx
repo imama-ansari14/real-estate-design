3c908ce2da7e9cfe13222781314c4f3795578081
Abiha-Sheikhani <abihajawed345@gmail.com>
2026-04-19 09:01:16 +0500

    Brief description of what you changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
-
-import "./App.css";
 import SectionLuxury from "./components/SectionLuxury";
 import ServicesShowcase from "./components/ServicesShowcase";
 import StatsShowcasePro from "./components/StatsShowcasePro";
+<<<<<<< HEAD
 import HowWeWork from "./components/HowWeWork";
 import ProjectsSection from "./components/ProjectsSection";
+=======
+import "./App.css";
+>>>>>>> 7abbe0db553466eb114c18410ec902a6b4bbc85f
 
 function App() {
   const [count, setCount] = useState(0);
